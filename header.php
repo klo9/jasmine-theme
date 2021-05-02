13a8832337d4548b0966aa29bc1c7b89f8f0cdeb
kl <kl@onesoftway.com>
2021-05-01 20:16:58 -0400

    + fonts

--- header.php
+++ header.php
@@ -5,6 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500&display=swap" rel="stylesheet"> 
+
     <?php wp_head();?>
 
 </head>
