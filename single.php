cdf78924cad0313e49b07a4aa47dc1be3a2c2816
kl <kl@onesoftway.com>
2021-05-03 17:38:20 -0400

    + single.php styles

--- single.php
+++ single.php
@@ -1,9 +1,18 @@
     <?php get_header();?>
 
-    <main class="container">
-        <div>
-            Hello
-        </div>        
+    <main class="container single">
+        <div class="single-post-img-container">
+            <?php the_post_thumbnail(); ?>
+        </div>
+        <article class="single-post-content-container">
+            <div class="single-post-title-container">
+                <?php the_title(); ?>
+            </div>
+            <div class="single-post-body-container">
+                <?php the_content(); ?>
+            </div>
+        </article>
+
     </main>
 
 </div>
