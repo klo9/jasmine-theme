5c32adf2f814f4c88d4c3f7851be5926a50f2a38
kl <kl@onesoftway.com>
2021-05-03 14:20:57 -0400

    + post-nav responsiveness

--- index.php
+++ index.php
@@ -6,35 +6,42 @@
             $wp_query = new WP_Query( array(
                 'posts_per_page' => 3,
                 'paged' => $paged
-                ) );
+            ) );
         ?>
             
-            <?php if( $wp_query->have_posts() ) : ?>
-                <ul>
+        <?php if( $wp_query->have_posts() ) : ?>
 
-                    <?php while( $wp_query->have_posts() ) : $wp_query->the_post() ?>
-                        <li>
-                            <div class="post-thumbnail"> <?php the_post_thumbnail() ?> </div>
-                            <div class="post-title"> <?php the_title() ?></div>
-                            <div class="post-excerpt"> <?php the_excerpt() ?></div>
-                        </li>
-                    <?php endwhile ?>
-                
-                </ul>
-                
-                <div class="post-nav row">
-                    <span class="prev-post"><?php echo get_previous_posts_link(__('Previous', 'textdomain')); ?></span>
-                    <span class="next-post"><?php echo get_next_posts_link(__('Next', 'textdomain')) ;?></span>
-                </div>
+        <ul>
 
-            <?php else : ?>
-                <div> No posts found! </div>
-            <?php endif ?>
+            <?php while( $wp_query->have_posts() ) : $wp_query->the_post() ?>
+                <li>
+                    <div class="post-thumbnail"> <?php the_post_thumbnail() ?> </div>
+                    <div class="post-title"> <?php the_title() ?></div>
+                    <div class="post-excerpt"> <?php the_excerpt() ?></div>
+                </li>
+            <?php endwhile ?>
         
-            <?php 
-                wp_reset_postdata();
-                $wp_query = $post_query; 
-            ?>
+        </ul>
+        
+        <div class="post-nav row">
+
+            <span class="prev-post"><?php echo get_previous_posts_link(__('Previous', 'textdomain')); ?></span>
+            <span class="next-post"><?php echo get_next_posts_link(__('Next', 'textdomain')) ;?></span>
+
+        </div>
+
+        <?php else : ?>
+
+            <div> No posts found! </div>
+
+        <?php endif ?>
+    
+        <?php 
+
+            wp_reset_postdata();
+            $wp_query = $post_query; 
+
+        ?>
 
     </main>
 </div>
