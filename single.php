7fb13bd93c9c6936c866d762be5706229f690a9a
kl <kl@onesoftway.com>
2021-05-04 18:07:59 -0400

    + single article styles

--- single.php
+++ single.php
@@ -1,14 +1,20 @@
     <?php get_header();?>
 
     <main class="container single">
-        <div class="single-post-img-container">
+        <div class="single-img-container">
             <?php the_post_thumbnail(); ?>
         </div>
-        <article class="single-post-content-container">
-            <div class="single-post-title-container">
-                <?php the_title(); ?>
+        <article class="single-content-container">
+            <div class="single-title-container">
+                <h1>
+                    <?php the_title(); ?>
+                </h1>
             </div>
-            <div class="single-post-body-container">
+            <p class="single-date-container">
+                <?php echo get_the_date( 'M j Y' ); ?>
+            </p>
+            <hr class="single-title-hr">
+            <div class="single-body-container">
                 <?php the_content(); ?>
             </div>
         </article>
