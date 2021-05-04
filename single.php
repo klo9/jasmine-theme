f2de7343fa20f7fa6dbfe09d72fcc4bd3707c025
kl <kl@onesoftway.com>
2021-05-04 19:14:15 -0400

    + author problems

--- single.php
+++ single.php
@@ -7,16 +7,19 @@
         <article class="single-content-container">
             <div class="single-title-container">
                 <h1>
-                    <?php the_title(); ?>
+                    <?php echo get_the_title(); ?>
                 </h1>
             </div>
             <p class="single-date-container">
                 <?php echo get_the_date( 'M j Y' ); ?>
             </p>
-            <hr class="single-title-hr">
+            <hr class="single-hr">
             <div class="single-body-container">
-                <?php the_content(); ?>
+                <?php echo get_the_content(); ?>
             </div>
+            <p>
+                <?php echo get_the_author(); ?>
+            </p>
         </article>
 
     </main>
