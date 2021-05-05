c7801910ea38b14c9ee112693ea30e15908f1e6e
kl <kl@onesoftway.com>
2021-05-05 18:38:05 -0400

    + author

--- single.php
+++ single.php
@@ -1,9 +1,13 @@
     <?php get_header();?>
 
+    <?php while ( have_posts() ) : the_post(); ?>
+
     <main class="container single">
+
         <div class="single-img-container">
             <?php the_post_thumbnail(); ?>
         </div>
+
         <article class="single-content-container">
             <div class="single-title-container">
                 <h1>
@@ -11,18 +15,20 @@
                 </h1>
             </div>
             <p class="single-date-container">
-                <?php echo get_the_date( 'M j Y' ); ?>
+                Published <?php echo get_the_date( 'M j Y' ); ?>
             </p>
             <hr class="single-hr">
             <div class="single-body-container">
                 <?php echo get_the_content(); ?>
             </div>
-            <p>
-                <?php echo get_the_author(); ?>
+            <hr class="single-hr">
+            <p class="single-author">
+                By: <?php echo get_the_author(); ?>
             </p>
         </article>
 
     </main>
+    <?php endwhile ?>
 
 </div>
 
