6670a7a87f635319d4a6c67c54bbfe389e2f5c2b
kl <kl@onesoftway.com>
2021-06-03 22:14:58 -0400

    + header title

--- header.php
+++ header.php
@@ -8,6 +8,8 @@
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,500;1,400&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@700&display=swap" rel="stylesheet"> 
+    
 
     <?php wp_head();?>
 
@@ -21,9 +23,14 @@
             // if ( function_exists('the_custom_logo') ) {
             //     the_custom_logo();
             // } else {
-                echo '<h1>' . esc_html(get_bloginfo('name')) . '</h1>';
+                // echo '<h1 class="header-title">' . esc_html(get_bloginfo('name')) . '</h1>';
             // }
         ?>
+        <div>
+            <h1 id="header-title">
+                <?php echo esc_html(get_bloginfo('name')); ?>
+            </h1>
+        </div>
         <nav class="jj-nav" id="jj-nav">
             <?php
                 wp_nav_menu( array(
